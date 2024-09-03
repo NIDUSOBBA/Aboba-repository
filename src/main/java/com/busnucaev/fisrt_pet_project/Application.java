c67fa873311a33fac5771730301cfd8031647594
NIDUSOBBA <abublikbrazers@gmail.com>
2024-09-03 14:49:55 +0300

    adding files

---
+++ src/main/java/com/busnucaev/fisrt_pet_project/Application.java
@@ -0,0 +1,14 @@
+package com.busnucaev.fisrt_pet_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
