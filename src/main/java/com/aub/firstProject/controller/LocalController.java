d4aac0b4a7b9531d7bb12f141cfc4590b9434f94
NIDUSOBBA <abublikbrazers@gmail.com>
2024-12-16 15:02:46 +0300

    Добавлен первый контроллер

---
+++ src/main/java/com/aub/firstProject/controller/LocalController.java
@@ -0,0 +1,14 @@
+package com.aub.firstProject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LocalController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello World";
+    }
+
+}
